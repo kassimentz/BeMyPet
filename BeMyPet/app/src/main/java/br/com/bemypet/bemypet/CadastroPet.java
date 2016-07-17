26ca355cab08b59758f5c4235c3cffd65304d345
Kassi <kassimentz@gmail.com>
2016-07-17 12:39:14 -0300

    tela de cadastroPet criada

---
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
@@ -0,0 +1,13 @@
+package br.com.bemypet.bemypet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroPet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pet);
+    }
+}
