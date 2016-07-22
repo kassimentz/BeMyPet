fd9531d1d35e4367a2194de0da57f0b19d3864f2
Kassiane Mentz <kassimentz@gmail.com>
2016-07-21 23:31:33 -0300

    remove all the layout tests

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
@@ -8,6 +8,6 @@ public class CadastroPet extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_pet_branco_azul_borda_lilas);
+        setContentView(R.layout.activity_cadastro_pet);
     }
 }
