597529ec118bcbbd5863825e753c67c247aa6a50
Kassiane Mentz <kassimentz@gmail.com>
2016-07-24 01:17:55 -0300

    navigation drawer

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
@@ -1,8 +1,10 @@
 package br.com.bemypet.bemypet;
 
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -17,10 +19,11 @@ public class CadastroPet extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //adiciona a seta para voltar para a mainActivity
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_cadastro_pet);
 
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
 
 
         spinEspecie = (Spinner) findViewById(R.id.spinEspecie);
