2dc3a6ec1e76dd4422f4402b3587e30e894b7785
Kassiane Mentz <kassimentz@gmail.com>
2016-07-23 23:04:40 -0300

    setting back button on cadastro pet and actionBar on main activity

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/CadastroPet.java
@@ -1,7 +1,10 @@
 package br.com.bemypet.bemypet;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -14,12 +17,19 @@ public class CadastroPet extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //adiciona a seta para voltar para a mainActivity
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_cadastro_pet);
+
+
 
         spinEspecie = (Spinner) findViewById(R.id.spinEspecie);
         spinEspecie.setAdapter(new ArrayAdapter<Especie>(this, android.R.layout.select_dialog_singlechoice, Especie.values()));
 
     }
+
+
+
 
 
 }
--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/MainActivity.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/MainActivity.java
@@ -1,7 +1,13 @@
 package br.com.bemypet.bemypet;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
     }
+
+    public void cadastroPet(View v){
+        Intent intent = new Intent(MainActivity.this, CadastroPet.class);
+        startActivity(intent);
+    }
+
+    //menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //ação de clique parar voltar para a mainActivity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Id correspondente ao botão Up/Home da actionbar
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.menuMoreOptions:
+                //chamar tela opcoes
+            case R.id.menuConfigSearch:
+                // chamar tela de configuracao de busca
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
