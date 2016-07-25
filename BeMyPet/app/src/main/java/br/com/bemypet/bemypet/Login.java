8ed6e4db7626f04bdc125ce84cf6e5a924108565
Kassiane Mentz <kassimentz@gmail.com>
2016-07-24 23:12:09 -0300

    update google signIn workin

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/Login.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -11,16 +12,41 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class Login extends AppCompatActivity {
 
     private CallbackManager callbackManager;
+    private GoogleApiClient mGoogleApiClient;
+    private final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder (GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        // Facebook Login
         callbackManager = CallbackManager.Factory.create();
 
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
@@ -30,11 +56,12 @@ public class Login extends AppCompatActivity {
             loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                 @Override
                 public void onSuccess(LoginResult loginResult) {
-                    Log.e("BeMyPet", "ToString: "+ loginResult.toString());
-                    Log.e("BeMyPet", "accessToken: "+ loginResult.getAccessToken());
-                    Log.e("BeMyPet", "token: "+ loginResult.getAccessToken().getToken());
-                    Log.e("BeMyPet", "granted: "+ loginResult.getRecentlyGrantedPermissions());
-                    Log.e("BeMyPet", "denied: "+ loginResult.getRecentlyGrantedPermissions());
+                    Log.i("BeMyPet", "ToString: "+ loginResult.toString());
+                    Log.i("BeMyPet", "accessToken: "+ loginResult.getAccessToken());
+                    Log.i("BeMyPet", "token: "+ loginResult.getAccessToken().getToken());
+                    Log.i("BeMyPet", "granted: "+ loginResult.getRecentlyGrantedPermissions());
+                    Log.i("BeMyPet", "denied: "+ loginResult.getRecentlyGrantedPermissions());
+
                     telaPrincipal();
                 }
 
@@ -57,8 +84,25 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Log.i("BeMyPet", "displayname: " + acct.getDisplayName());
+            Log.i("BeMyPet", "displayname: " + acct.getEmail());
+
+            telaPrincipal();
+        } else {
+            Log.d("CURSO", "sign out");
+        }
     }
 
     public void telaPrincipal(){
