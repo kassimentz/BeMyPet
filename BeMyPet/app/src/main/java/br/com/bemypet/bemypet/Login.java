f5e17e3fbcf461cd8f90c8ef853493f52d6ab41d
Kassiane Mentz <kassimentz@gmail.com>
2016-07-28 23:31:36 -0300

    get facebook data

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/Login.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/Login.java
@@ -10,6 +10,8 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
@@ -17,6 +19,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Login extends AppCompatActivity {
 
@@ -53,17 +64,36 @@ public class Login extends AppCompatActivity {
         if(accessToken == null){
 
             LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+            List<String> permissions = new ArrayList<>();
+            permissions.add("public_profile");
+            permissions.add("email");
+            permissions.add("user_birthday");
+
+            loginButton.setReadPermissions(permissions);
+
             loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
                 @Override
                 public void onSuccess(LoginResult loginResult) {
-                    Log.i("BeMyPet", "ToString: "+ loginResult.toString());
-                    Log.i("BeMyPet", "accessToken: "+ loginResult.getAccessToken());
-                    Log.i("BeMyPet", "token: "+ loginResult.getAccessToken().getToken());
-                    Log.i("BeMyPet", "granted: "+ loginResult.getRecentlyGrantedPermissions());
-                    Log.i("BeMyPet", "denied: "+ loginResult.getRecentlyGrantedPermissions());
 
+                    GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback(){
+                        @Override
+                        public void onCompleted(JSONObject object, GraphResponse response) {
+                            Log.i("BeMyPet", "ToString: "+ response.toString());
+                            // get facebook data from login
+                            Bundle bFacebookData = getFacebookData(object);
+                            //{"id":"1138061596265568","first_name":"Kassiane","last_name":"Mentz","email":"kassimentz@gmail.com","gender":"female","birthday":"01\/08\/1987"}
+                            //redirecionar aqui passando o bundle para a proxima activity
+
+                        }
+                    });
+
+                    Bundle parameters = new Bundle();
+                    parameters.putString("fields", "id, first_name, last_name, email, gender, birthday, location"); // Parametros que quero
+                    request.setParameters(parameters);
+                    request.executeAsync();
                     telaPrincipal();
                 }
+
 
                 @Override
                 public void onCancel() {
@@ -81,6 +111,46 @@ public class Login extends AppCompatActivity {
         }
 
     }
+
+    private Bundle getFacebookData(JSONObject object) {
+
+        try {
+            Bundle bundle = new Bundle();
+            String id = object.getString("id");
+
+            try {
+                URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("location"))
+                bundle.putString("location", object.getJSONObject("location").getString("name"));
+
+            return bundle;
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
