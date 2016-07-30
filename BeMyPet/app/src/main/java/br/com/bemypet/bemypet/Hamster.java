ad52d9de3cfb1aeafa720650234c960f8a4c5456
Kassi <kassimentz@gmail.com>
2016-07-30 17:12:41 -0300

    add classes

---
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/Hamster.java
@@ -0,0 +1,21 @@
+package br.com.bemypet.bemypet;
+
+/**
+ * Created by Cassio on 30/07/16.
+ */
+public class Hamster extends Pet {
+
+    private Boolean castrado;
+
+    public Hamster(){
+
+    }
+
+    public Boolean getCastrado() {
+        return castrado;
+    }
+
+    public void setCastrado(Boolean castrado) {
+        this.castrado = castrado;
+    }
+}
---
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/Passaro.java
@@ -0,0 +1,21 @@
+package br.com.bemypet.bemypet;
+
+/**
+ * Created by Cassio on 30/07/16.
+ */
+public class Passaro extends Pet {
+
+    private Boolean castrado;
+
+    public Passaro(){
+
+    }
+
+    public Boolean getCastrado() {
+        return castrado;
+    }
+
+    public void setCastrado(Boolean castrado) {
+        this.castrado = castrado;
+    }
+}
