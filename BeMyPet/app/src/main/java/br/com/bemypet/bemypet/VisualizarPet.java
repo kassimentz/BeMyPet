a2f45f216a56cd758eb86ca9137d6a658289be0c
Kassi <kassimentz@gmail.com>
2016-08-20 18:04:18 -0300

    add ViewPager and imageadapter

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/VisualizarPet.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/VisualizarPet.java
@@ -1,7 +1,10 @@
 package br.com.bemypet.bemypet;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.bemypet.bemypet.adapter.ImageAdapter;
 
 public class VisualizarPet extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class VisualizarPet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizar_pet);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
     }
+
 }
---
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/adapter/ImageAdapter.java
@@ -0,0 +1,50 @@
+package br.com.bemypet.bemypet.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import br.com.bemypet.bemypet.R;
+
+/**
+ * Created by Cassio on 20/08/16.
+ */
+public class ImageAdapter extends PagerAdapter {
+    Context context;
+    private int[] GalImages = new int[] {
+            android.R.drawable.btn_star,
+            android.R.drawable.btn_plus,
+            android.R.drawable.ic_dialog_info
+    };
+    public ImageAdapter(Context context){
+        this.context=context;
+    }
+    @Override
+    public int getCount() {
+        return GalImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        int padding = context.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
+        imageView.setPadding(padding, padding, padding, padding);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageResource(GalImages[position]);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
+}
