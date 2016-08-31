5f918fad7c946808959bae212e1cd583a4e425cf
kassiane.mentz <kassiane.mentz@objectedge.com>
2016-08-31 14:01:45 -0300

    listview added

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/ListaNotificacoes.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/ListaNotificacoes.java
@@ -2,12 +2,65 @@ package br.com.bemypet.bemypet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.bemypet.bemypet.adapter.RecyclerViewAdapter;
+import br.com.bemypet.bemypet.api.StringUtils;
+import br.com.bemypet.bemypet.controller.Constants;
+import br.com.bemypet.bemypet.controller.ManagerPreferences;
+import br.com.bemypet.bemypet.model.Notificacao;
+
+
 
 public class ListaNotificacoes extends AppCompatActivity {
+    String usuarioCpf;
+    List<Notificacao> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notificacoes);
+
+        if(!StringUtils.isNullOrEmpty(ManagerPreferences.getString(this, Constants.USUARIO_CPF))) {
+            usuarioCpf = ManagerPreferences.getString(this, Constants.USUARIO_CPF);
+        }
+
+        data = getNotificacoes();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(data, getApplication());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+
+    private List<Notificacao> getNotificacoes() {
+
+        DatabaseReference myRef = CadastroUsuario.dbRef.child("usuario").child(usuarioCpf).getRef();
+        Query query = myRef.orderByChild("notificacoes");
+
+        query.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()){
+                    data.add(snap.getValue(Notificacao.class));
+                }
+            }
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        return null;
+    }
+
+
 }
---
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/adapter/RecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package br.com.bemypet.bemypet.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Collections;
+import java.util.List;
+
+import br.com.bemypet.bemypet.R;
+import br.com.bemypet.bemypet.model.Notificacao;
+
+/**
+ * Created by objectedge on 8/31/16.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+    List<Notificacao> list = Collections.emptyList();
+    Context context;
+
+    public RecyclerViewAdapter(List<Notificacao> list, Context context){
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Inflate the layout, initialize the View Holder
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        //Use the provided View Holder on the onCreateViewHolder method to populate the current row on the RecyclerView
+        holder.txtAdotante.setText(list.get(position).getCpfAdotante());
+        holder.txtDoador.setText(list.get(position).getCpfDoador());
+        holder.imageView.setImageResource(list.get(position).getImage().charAt(0));
+
+        //animate(holder);
+    }
+
+    @Override
+    public int getItemCount() {
+        //returns the number of elements the RecyclerView will display
+        return list.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    // Insert a new item to the RecyclerView on a predefined position
+    public void insert(int position, Notificacao data) {
+        list.add(position, data);
+        notifyItemInserted(position);
+    }
+
+    // Remove a RecyclerView item containing a specified Data object
+    public void remove(Notificacao data) {
+        int position = list.indexOf(data);
+        list.remove(position);
+        notifyItemRemoved(position);
+    }
+
+}
