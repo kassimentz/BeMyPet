73a5c9d675ac84f03a0234a39b943ab99c33dbe7
kassiane.mentz <kassiane.mentz@objectedge.com>
2016-09-02 14:49:47 -0300

    add method verificaNotificacao

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/adapter/RecyclerViewAdapter.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/adapter/RecyclerViewAdapter.java
@@ -3,20 +3,30 @@ package br.com.bemypet.bemypet.adapter;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import br.com.bemypet.bemypet.CadastroUsuario;
 import br.com.bemypet.bemypet.R;
+import br.com.bemypet.bemypet.api.StringUtils;
+import br.com.bemypet.bemypet.controller.Constants;
+import br.com.bemypet.bemypet.controller.ManagerPreferences;
 import br.com.bemypet.bemypet.model.Notificacao;
+import br.com.bemypet.bemypet.model.Usuario;
 
 /**
  * Created by objectedge on 8/31/16.
@@ -26,10 +36,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     List<Notificacao> list = Collections.emptyList();
     Context context;
+    String cpfLogado;
+    List<Usuario> usuarioList = new ArrayList<>();
 
     public RecyclerViewAdapter(List<Notificacao> list, Context context){
         this.list = list;
         this.context = context;
+
+        cpfLogado = ManagerPreferences.getString(context, Constants.USUARIO_CPF);
+
     }
 
     @Override
@@ -52,14 +67,32 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.setClickListener(new ItemClickListener() {
             @Override
             public void onClick(View view, int position, boolean isLongClick) {
-                if (isLongClick) {
-                    Toast.makeText(context, "#" + position + " - " + list.get(position) + " (Long click)", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(context, "#" + position + " - " + list.get(position), Toast.LENGTH_SHORT).show();
-                }
+                verificaNotificacao(list.get(position));
             }
         });
 
+    }
+
+
+    private void verificaNotificacao(Notificacao notificacao) {
+        if(notificacao.getStatusNotificacao().equalsIgnoreCase("queroAdotar")){
+
+            if(cpfLogado.equalsIgnoreCase(notificacao.getCpfAdotante())){
+                //ao clicar mostrar Data de aprovação, nome do usuario que adotou, nome e foto do pet.
+            }else{
+                //clicar mostrar o perfil do Usuário adotante (mesmos passos para aprovar adoção)
+                //(verificar o que precisa receber de bundle)
+            }
+        }else if(notificacao.getStatusNotificacao().equalsIgnoreCase("adocaoAprovada")){
+            //fazer outro if para o tipo de usuario -- buscar usuario
+            if(cpfLogado.equalsIgnoreCase(notificacao.getCpfAdotante())){
+                //mostrando a rota para buscar o pet - verificar o que precisa passar de bunle
+            }else{
+                //ao clicar mostrar Data de aprovação, nome do usuario que adotou, nome e foto do pet.
+            }
+        }else if (notificacao.getStatusNotificacao().equalsIgnoreCase("adocaoReprovada")){
+            //ao clicar mostrar Data de aprovação, nome do usuario que adotou, nome e foto do pet.
+        }
     }
 
     @Override
@@ -128,4 +161,5 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public interface ItemClickListener {
         void onClick(View view, int position, boolean isLongClick);
     }
+
 }
