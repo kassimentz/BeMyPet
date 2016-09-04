19db0bbd7c82dec2cf5be15262d60d0510f9d5bb
Kassiane Mentz <kassimentz@gmail.com>
2016-09-03 23:55:38 -0300

    add contex from view on Recycler view adapter

--- BeMyPet/app/src/main/java/br/com/bemypet/bemypet/adapter/RecyclerViewAdapter.java
+++ BeMyPet/app/src/main/java/br/com/bemypet/bemypet/adapter/RecyclerViewAdapter.java
@@ -74,7 +74,7 @@ public class RecyclerViewAdapter extends FirebaseRecyclerAdapter<Notificacao, No
         viewHolder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                verificaNotificacao(notificacao);
+                verificaNotificacao(view, notificacao);
             }
         });
 
@@ -82,43 +82,43 @@ public class RecyclerViewAdapter extends FirebaseRecyclerAdapter<Notificacao, No
 
 
 
-    private void verificaNotificacao(Notificacao notificacao) {
+    private void verificaNotificacao(View v, Notificacao notificacao) {
 
         if(cpfLogado.equalsIgnoreCase(notificacao.getCpfAdotante())){
             //quem está logado é adotante
             if(notificacao.getStatusNotificacao().equalsIgnoreCase(Constants.QUERO_ADOTAR)){
                 //é adotante e notificacao é "quero adotar", entao ve a notificacao padrao
-                goToVisualizarNotificacaoPadrao(notificacao);
+                goToVisualizarNotificacaoPadrao(v, notificacao);
             }else if(notificacao.getStatusNotificacao().equalsIgnoreCase(Constants.ADOCAO_APROVADA)){
                 //é adotante e notificacao é "adocao aprovada", entao visualiza rota para buscar pet
-                goToVisualizarRotaPet(notificacao);
+                goToVisualizarRotaPet(v, notificacao);
             }else if(notificacao.getStatusNotificacao().equalsIgnoreCase(Constants.ADOCAO_REPROVADA)){
                 //é adotante e notificacao é "adocao reprovada"
-                goToVisualizarNotificacaoPadrao(notificacao);
+                goToVisualizarNotificacaoPadrao(v, notificacao);
             }
         }else{
             //quem está logado é doador
             if(notificacao.getStatusNotificacao().equalsIgnoreCase(Constants.QUERO_ADOTAR)){
                 //é doador e notificacao é "quero adotar", entao ve o perfil do adotante
-                goToVisualizarUsuario(notificacao);
+                goToVisualizarUsuario(v, notificacao);
             }else if(notificacao.getStatusNotificacao().equalsIgnoreCase(Constants.ADOCAO_APROVADA)){
                 //é doador e notificacao é "adocao aprovada", vai para visualizacao padrao
-                goToVisualizarNotificacaoPadrao(notificacao);
+                goToVisualizarNotificacaoPadrao(v, notificacao);
             }else if(notificacao.getStatusNotificacao().equalsIgnoreCase(Constants.ADOCAO_REPROVADA)){
                 //é doador e notificacao é "adocao reprovada"
-                goToVisualizarNotificacaoPadrao(notificacao);
+                goToVisualizarNotificacaoPadrao(v, notificacao);
             }
         }
 
 
     }
 
-    private void goToVisualizarUsuario(Notificacao notificacao) {
+    private void goToVisualizarUsuario(View v, Notificacao notificacao) {
         String cpfAdotante = null;
         if(notificacao.getCpfAdotante() != null){
             cpfAdotante = notificacao.getCpfAdotante();
             Bundle bundle = new Bundle();
-            Intent resultIntent = new Intent(context, VisualizarUsuario.class);
+            Intent resultIntent = new Intent(v.getContext(), VisualizarUsuario.class);
             bundle.putString("cpfAdotante", cpfAdotante);
             bundle.putString("idPet", notificacao.getIdPet());
             resultIntent.putExtras(bundle);
@@ -126,7 +126,7 @@ public class RecyclerViewAdapter extends FirebaseRecyclerAdapter<Notificacao, No
             context.startActivity(resultIntent);
         }else{
 
-            Intent resultIntent = new Intent(context, MainActivity.class);
+            Intent resultIntent = new Intent(v.getContext(), MainActivity.class);
             Bundle bundle = new Bundle();
             bundle.putString("erroAdotante", "erroAdotante");
             resultIntent.putExtras(bundle);
@@ -137,9 +137,9 @@ public class RecyclerViewAdapter extends FirebaseRecyclerAdapter<Notificacao, No
 
     }
 
-    private void goToVisualizarRotaPet(Notificacao notificacao) {
+    private void goToVisualizarRotaPet(View v, Notificacao notificacao) {
         Bundle bundle = new Bundle();
-        Intent resultIntent = new Intent(context, VisualizarRotaPetActivity.class);
+        Intent resultIntent = new Intent(v.getContext(), VisualizarRotaPetActivity.class);
         bundle.putString("origem", notificacao.getEnderecoAdotante());
         bundle.putString("destino", notificacao.getEnderecoDoador());
         resultIntent.putExtras(bundle);
@@ -147,9 +147,9 @@ public class RecyclerViewAdapter extends FirebaseRecyclerAdapter<Notificacao, No
         context.startActivity(resultIntent);
     }
 
-    private void goToVisualizarNotificacaoPadrao(Notificacao notificacao) {
+    private void goToVisualizarNotificacaoPadrao(View v, Notificacao notificacao) {
         Bundle bundle = new Bundle();
-        Intent resultIntent = new Intent(context, VisualizarNotificacaoPadrao.class);
+        Intent resultIntent = new Intent(v.getContext(), VisualizarNotificacaoPadrao.class);
         bundle.putLong("data", notificacao.getData());
         bundle.putString("adotante", notificacao.getNomeAdotante());
         bundle.putString("doador", notificacao.getNomeDoador());
